e37674f0c3bcb320233bd3c6b765a1919e82247e
omegafishy <zhiyulling@gmail.com>
2019-01-02 16:53:35 +0800

    Updates

---
+++ precache-manifest.ea5188f98fa6b6243515ac003a3ffde6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2194e560862208ff77cd",
+    "url": "/btoviewer/static/js/runtime~main.2194e560.js"
+  },
+  {
+    "revision": "7ce4bbe1c279d6bb202b",
+    "url": "/btoviewer/static/js/main.7ce4bbe1.chunk.js"
+  },
+  {
+    "revision": "fb11c852a8f184914dd0",
+    "url": "/btoviewer/static/js/1.fb11c852.chunk.js"
+  },
+  {
+    "revision": "7ce4bbe1c279d6bb202b",
+    "url": "/btoviewer/static/css/main.59950897.chunk.css"
+  },
+  {
+    "revision": "fb11c852a8f184914dd0",
+    "url": "/btoviewer/static/css/1.891b7bce.chunk.css"
+  },
+  {
+    "revision": "b2ab04da2453350743270ccf21be297d",
+    "url": "/btoviewer/index.html"
+  }
+];
